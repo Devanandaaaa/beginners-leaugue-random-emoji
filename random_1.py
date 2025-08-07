46d6e5a7e02edcc0026805febcd69517f658791d
Devanandaaaa <devananda.2212@gmail.com>
2025-08-07 16:41:28 +0530

    random emoji generator

---
+++ random_1.py
@@ -0,0 +1,10 @@
+import random
+
+emojis = ["😀", "😂", "😍", "🥺", "😎", "🤖", "🔥", "🌈", "🎉", "🐶", "🍕", "🚀", "🧠", "🌍", "🎮"]
+
+random_emoji = random.choice(emojis)
+
+
+print("Your random emoji is:", random_emoji)
+
+
